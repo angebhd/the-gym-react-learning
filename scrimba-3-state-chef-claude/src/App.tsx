cce80a7afbf05824093744800728f8f5da001935
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2025-07-16 09:04:07 +0200

    Adding scrimba 3

---
+++ scrimba-3-state-chef-claude/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Form from './components/Form'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Form />
+      </main>
+      
+    </>
+  )
+}
+
+export default App
---
+++ scrimba-3-state-chef-claude/src/components/StateExo.tsx
@@ -0,0 +1,7 @@
+export default function StateExo() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
