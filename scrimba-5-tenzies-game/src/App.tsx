13536d971a55d76f78c22a54ccde507e8a826b3a
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2025-07-29 10:37:48 +0200

    Scrimba 05 - Capstone Project 1: Tenzies Completed

--- scrimba-5-tenzies-game/src/App.tsx
+++ scrimba-5-tenzies-game/src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Die from './components/Die';
+import ReactConfetti from 'react-confetti';
 
 export type DieType = {
   id: number
@@ -9,34 +10,29 @@ export type DieType = {
   hold: Function
 }
 
-function App() {
-  const [dice, setDice] = useState<DieType[]>(generateAllNewDice());
-  let gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value);
-
-
+export default function App() {
+  const [dice, setDice] = useState<DieType[]>(() => generateAllNewDice()); // Lazy state initialization
+  const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   function generateAllNewDice(): DieType[] {
     const arr: DieType[] = [];
     for (let i = 0; i < 10; i++) arr.push({ id: i, value: Math.ceil(Math.random() * 6), isHeld: false, hold });
-
     return arr;
-    // return new Array(10)
-    //         .fill(0)
-    //         .map(() => Math.ceil(Math.random() * 6))
-
   }
   const rollDice = () => {
-    const newArr: DieType[] = []
-    for (let die of dice) {
-      newArr.push({ ...die, value: die.isHeld ? die.value : Math.ceil(Math.random() * 6) });
-    }
-    setDice(newArr);
+    if (!gameWon) {
 
-    // setDice(oldDice => oldDice.map(die =>
-    //   die.isHeld ?
-    //     die :
-    //     { ...die, value: Math.ceil(Math.random() * 6) }
-    // ))
+      const newArr: DieType[] = []
+      for (let die of dice) {
+        newArr.push({ ...die, value: die.isHeld ? die.value : Math.ceil(Math.random() * 6) });
+      }
+      setDice(newArr);
+    } else {
+      setDice(generateAllNewDice());
+    }
+
+
   }
   function hold(id: number) {
     setDice(prevDice => {
@@ -46,10 +42,19 @@ function App() {
     })
   }
 
-  console.log(generateAllNewDice());
+  useEffect(() => {
+    if (gameWon) {
+      buttonRef.current?.focus()
+    }
+  }, [gameWon])
 
   return (
     <main className="bg-[#f5f5f5] h-full rounded-lg flex flex-col justify-evenly items-center">
+      {gameWon && <ReactConfetti />}
+      {/* for screen reader  */}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+      </div>
       <div>
         <h1 className=" text-2xl text-center">Tenzies</h1>
         <p className=" font-light text-center ">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
@@ -59,11 +64,10 @@ function App() {
         {dice.map((dieObject, ind) => <Die key={ind} dieData={dieObject} />)}
 
       </div>
-      <button onClick={rollDice}
+      <button onClick={rollDice} ref={buttonRef}
         className='h-[50px] px-3 border-none rounded-lg bg-[#5035ff] text-white text-lg cursor-pointer   '
       >{gameWon ? "New Game" : "Roll"}</button>
     </main>
   )
 }
 
-export default App
--- scrimba-5-tenzies-game/src/components/Die.tsx
+++ scrimba-5-tenzies-game/src/components/Die.tsx
@@ -1,12 +1,16 @@
 import type { DieType } from "../App";
 
-export default function Die({dieData}: { dieData: DieType }) {
+export default function Die({ dieData }: { dieData: DieType }) {
     return (
-        <button 
-        className={`h-[50px] w-[50px] rounded-lg shadow-xl border-none  text-xl font-bold cursor-pointer
+        <button
+            className={`h-[50px] w-[50px] rounded-lg shadow-xl border-none  text-xl font-bold cursor-pointer
             ${dieData.isHeld ? "bg-[#59E391]" : "bg-white"}
             `}
-        onClick={() => dieData.hold(dieData.id)}
+            onClick={() => dieData.hold(dieData.id)}
+
+            aria-pressed={dieData.isHeld}
+            aria-label={`Die with value ${dieData.value}, 
+            ${dieData.isHeld ? "held" : "not held"}`}
         >
             {dieData.value}
         </button>
